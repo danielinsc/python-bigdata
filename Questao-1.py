c37020b389e54e390347af0498f98fba68f29ecf
fernandomeissner <84792450+fernandomeissner@users.noreply.github.com>
2022-11-09 22:40:30 -0300

    Create Questao-1.py

---
+++ Questao-1.py
@@ -0,0 +1,14 @@
+'''1) Faça função que calcule a área do trapézio, dados:a) Base maior
+b) Base menor
+c) Altura
+Lembrando que a área pode ser calculada por:
+O programa principal deve pedir os valores e usar a função para calcular a área.'''
+
+B = float(input('Qual o valor da base maior? '))
+b = float(input('Qual o valor da base menor? '))
+c = float(input('Qual o valor da altura? '))
+a = 2
+
+formula = ((B = b) * h) / a
+
+print("A área do trapézio é: ", formula)
