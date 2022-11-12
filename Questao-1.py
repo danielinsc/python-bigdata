b565873fb4ea3cec68d4ce41bf507bed28039318
fernandomeissner <84792450+fernandomeissner@users.noreply.github.com>
2022-11-12 17:17:38 -0300

    Update Questao-1.py

--- Questao-1.py
+++ Questao-1.py
@@ -4,11 +4,11 @@ c) Altura
 Lembrando que a área pode ser calculada por:
 O programa principal deve pedir os valores e usar a função para calcular a área.'''
 
-B = float(input('Qual o valor da base maior? '))
-b = float(input('Qual o valor da base menor? '))
-h = float(input('Qual o valor da altura? '))
+B = float(input('Qual o valor da base maior? '))  # solicita informacao
+b = float(input('Qual o valor da base menor? ')) # solicita informacao
+h = float(input('Qual o valor da altura? ')) # solicita informacao
 a = 2
 
-formula = ((B + b) * h) / a
+formula = ((B + b) * h) / a # realizacao do calculo
 
-print("A área do trapézio é: ", formula)
+print("A área do trapézio é: ", formula) # printa na tela o calculo
